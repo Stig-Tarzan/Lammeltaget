f378c8e34223b3036dde4a2ee7fb20fafe4bbc4e
Stig-Tarzan <john.f.almqvist@gmail.com>
2017-05-18 13:51:52 +0200

    Register

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,4 +1,3 @@
 <?php
 
 include 'models/db.inc.php';
-include_once 'models/register_process.inc.php';
--- includes/models/register_process.inc.php
+++ includes/models/register_process.inc.php
@@ -2,11 +2,11 @@
 	include '../bootstrap.php';
 
 	$user_name = $_POST['username_value'];
-	$email = $_POST['email_value'];
+	$email = $_POST['email_value'];				
 	$password = $_POST['password_value'];
 
 	$sql = "INSERT INTO user (userName, email, password) 
-				VALUES ('$user_name', '$email', '$password')";
+				VALUES ('$user_name', '$email', '$password')";					
 	
 	$result = mysqli_query($conn, $sql);
 	if ($result) 
