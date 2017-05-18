a23191f1d29a12a4b73da7f6a8ca5dcea7226ecf
Stig-Jane <hnordlander@gmail.com>
2017-05-18 11:41:42 +0200

    hankthings

--- includes/views/login.php
+++ includes/views/login.php
@@ -3,5 +3,5 @@
 	$login_fields = "<input name='username_value' id='username_input' placeholder='Username'>
 					 <input name='pasword_value' id='password_input' type='password' placeholder='Password'>
 					 <button name='login_button' id='login_button' class='button'>Logga in</button>
-					 <a><p>Registrera</p></a>";
+					 <a id='register_text'><p>Registrera</p></a>";
 	echo $login_fields;
---
+++ includes/views/register.php
@@ -0,0 +1,6 @@
+<?php
+
+	$register_fields = "<input name='username_value' id='username_register' placeholder='Välj användarnamn'>
+						<input name='mail_value' id='mail_register' placeholder='e-mail'>
+						<input name='password_value' id='pasword_register' type='password' placeholder='Välj lösenord'>
+						<input name='reenter_password_value' id='reenter_password_register' placeholder='Upprepa lösenord'>"
