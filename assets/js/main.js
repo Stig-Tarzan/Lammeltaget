63ca2cefdae8d2ec98e65219f0a93f026d6e7d94
Stig-Tarzan <john.f.almqvist@gmail.com>
2017-05-22 13:43:28 +0200

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,21 +121,21 @@ function updateTextInput(val) {
           document.getElementById('trail_current_difficulty').value=val; 
         }
 
-function getDistance(point1, point2)
+function getDistance(lat1, lat2, lon1, lon2)
 {
-	ar R = 6371e3; // metres
-	ar φ1 = lat1.toRadians();
-	ar φ2 = lat2.toRadians();
-	ar Δφ = (lat2-lat1).toRadians();
-	ar Δλ = (lon2-lon1).toRadians();
+	var R = 6371e3; // metres
+	var φ1 = lat1.toRadians();
+	var φ2 = lat2.toRadians();
+	var Δφ = (lat2-lat1).toRadians();
+	var Δλ = (lon2-lon1).toRadians();
 
-	ar a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
 	       Math.cos(φ1) * Math.cos(φ2) *
 	       Math.sin(Δλ/2) * Math.sin(Δλ/2);
-	ar c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-	ar d = R * c;
-	return 
+	var d = R * c;
+	return d;
 }
 
 function updateLenghtInput(val) {
