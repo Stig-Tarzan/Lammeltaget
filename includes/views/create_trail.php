238c1ed82e769d7d7ec6791e3bc7f2af919dc454
Stig-Jane <hnordlander@gmail.com>
2017-05-22 10:45:01 +0200

    a

--- includes/views/create_trail.php
+++ includes/views/create_trail.php
@@ -5,12 +5,7 @@
 
  -->
 <?php
- 	 $googlemaps_api = '<iframe id="google_map" src="//www.google.com/maps/embed/v1/place?q=Empire%20State%20Building
-						   	&zoom=13
-						    &attribution_source=Google+Maps+Embed+API
-						    &attribution_web_url=https://developers.google.com/maps/documentation/embed/
-						    &key=AIzaSyBdgE6eNr-rgeTfeOn-RFf7hjuY4stagvc" allowfullscreen>
-						</iframe>';
+
 
  	 $googlemaps_api = "
  	 					<div id='map'></div>
