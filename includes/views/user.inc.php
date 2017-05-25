ad29b343d792d618c8828c310d59c2970ec80b03
Stig-Jane <hnordlander@gmail.com>
2017-05-25 12:13:14 +0200

    whitep

--- includes/views/user.inc.php
+++ includes/views/user.inc.php
@@ -4,8 +4,8 @@ session_start();
 	if(isset($_SESSION['user_name']))
 	{
 		echo "
-				<div id='username_button'></div>
-				<a id='log_out_button'>Logga ut ".$_SESSION['user_name']."</a>
+				<div  id='username_button'></div>
+				<a class='banner_obj' id='log_out_button'>Logga ut ".$_SESSION['user_name']."</a>
 				
 				";	
 	}
