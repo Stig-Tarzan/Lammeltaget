3130ccc42a0439077b3840286e753275671d49a3
Stig-Jane <hnordlander@gmail.com>
2017-05-28 13:48:03 +0200

    11

--- includes/views/profile.inc.php
+++ includes/views/profile.inc.php
@@ -9,15 +9,34 @@ session_start();
 		$steps = 0;
 		
 
-		$sql = "SELECT upVotes,downVotes FROM trail WHERE trail.userID=$loged_in";
-		$result = mysqli_query($conn, $sql);
 
-		while ($row = $result->fetch_assoc())  //Runs through the entire result
+	$sql = "SELECT * FROM trail,user WHERE trail.userID=user.userID";
+	$result = mysqli_query($conn, $sql);
+
+
+	
+//Ovan bör ligga kvar och $result bör "Returnas"
+
+//Vi bör bryta ut nedan till en view
+		while ($row = $result->fetch_assoc())  
 		{
-			$steps = $steps + ($row['upVotes']-$row['downVotes']);
+			$trail_name = $row['trailName'];
+
+			$sql_trail_id = "SELECT * FROM trail WHERE trailName='$trail_name'";
+			$result_trail_id=mysqli_query($conn, $sql_trail_id);
+			$row_trail_id=mysqli_fetch_assoc($result_trail_id);
+			$trail_id = $row_trail_id['trailID'];
+
+			$sql_rating = "SELECT SUM(vote) as 'rating' FROM vote WHERE trailID = '$trail_id'";
+			$result_rating = mysqli_query($conn, $sql_rating);
+			$row_rating = mysqli_fetch_assoc($result_rating);
+
+
+			if( $row['userID'] == $loged_in) {
+				$steps = $steps + $row_rating['rating'];
+			}
 
 		}
-
 		echo "
 				<div id='username_welcome'>" .$_SESSION['user_name'] ."<a id='user_steg'>Steg " .$steps ."</a></div>
 				<a id='user_trails'>Mina Leder</a>
--- includes/views/register.inc.php
+++ includes/views/register.inc.php
@@ -6,9 +6,16 @@
 
 
 								<input name='username_value' id='username_register' placeholder='Välj användarnamn'>
-								<input name='email_value' id='email_register' placeholder='e-mail'>
+								<input name='email_value' id='email_register' type='
+								e-mail' placeholder='e-mail'>
+								<input name='first_name' id='first_name_register'  placeholder='Namn'>
+								<input name='last_name' id=last_name_register'  placeholder='Efternamn'>
+								<input name='dob_value' id='dob_register' type='date'  >
+								<input name='address_value' id='dob_register' placeholder='Gatunamn'>
+								<input name='postcode_value' id='postcode_register' placeholder='Postnummer'>
+								<input name='city_value' id='city_register' placeholder='Stad'>
 								<input name='password_value' id='password_register' type='password' placeholder='Välj lösenord'>
-								<input name='reenter_password_value' id='reenter_password_register' placeholder='Upprepa lösenord'>
+								<input name='reenter_password_value' id='reenter_password_register' type='password' placeholder='Upprepa lösenord'>
 								<button id='apply_register_button' class='button'> Registrera användare</button>
 							";
 
