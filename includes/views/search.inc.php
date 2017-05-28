d1392815aa57bcad481da615cae3f9183d111b2d
Stig-Jane <hnordlander@gmail.com>
2017-05-28 17:13:28 +0200

    123

--- includes/views/search.inc.php
+++ includes/views/search.inc.php
@@ -1,7 +1,7 @@
 <?php
 	echo "
 			
-				<input name='search_value' id='search_input' placeholder='Enter searchphrase'>
+				<input name='search_value' id='search_input' type='text' placeholder='Enter searchphrase'>
 				
-				<button type='button' name='search_button' id='search_button' class='button'>Search</button>
+				<button type='button' name='search_button' id='search_button' class='button'>Sök</button>
 				";
