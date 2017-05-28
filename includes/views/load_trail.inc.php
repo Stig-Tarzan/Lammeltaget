682a5a11e5fb28ffd07295cac3f322128abc5737
Stig-Tarzan <john.f.almqvist@gmail.com>
2017-05-28 19:35:35 +0200

    123

--- includes/views/load_trail.inc.php
+++ includes/views/load_trail.inc.php
@@ -12,8 +12,16 @@
   $trail_name = $_POST['trail_name'];
   $sql = "SELECT * FROM trail WHERE trailName = '$trail_name'";
   $result = mysqli_query($conn, $sql);
+
+  $sql_vote = "SELECT * FROM user,trail join (SELECT trail.trailID, SUM(vote) as 'rating' FROM vote,trail WHERE vote.trailID = trail.trailID group by trail.trailID) rate on trail.trailID = rate.trailID WHERE trail.userID=user.userID AND trail.trailName = '$trail_name'";
+  $result_vote = mysqli_query($conn, $sql_vote);
+  $row_vote = mysqli_fetch_assoc($result_vote);
+  $rating = $row_vote['rating'];
+
+
   $row = mysqli_fetch_assoc($result);
   $trail_id = $row['trailID'];
+  $trail_description = $row['trailInfoText'];
 
   
   $googlemaps_api = "
@@ -72,10 +80,12 @@ $('#add_trail').css('bottom', '15%');
             ";
 
   echo $googlemaps_api;
-  echo "<input type='hidden' id='vote_trail_name' value='$trail_name'></input>";
-  echo "<input type='hidden' id='vote_trail_creator_name' value='$trail_creator'></input>";
+  echo "<label id='vote_trail_name' value='$trail_name'><p>$trail_name</p></label>";
+  echo "<label id='vote_trail_creator_name' value='$trail_creator'><p>Skapad av: $trail_creator</p></label>"; 
+  echo "<text id='vote_trail_name' value='$trail_name'><p>Beskrivning: $trail_description</p></text>";
   echo "<div id=vote_container>";
   echo "<i id='upvote_trail_button'class='material-icons-arrows'>arrow_upward</i>";
+  echo "<label id='trail_rating'><p>$rating</p></label>";
   echo "<i id='downvote_trail_button' class='material-icons-arrows'>arrow_downward</i></div>";
 
   $user_id = $_SESSION['user_id'];
@@ -90,7 +100,7 @@ $('#add_trail').css('bottom', '15%');
     $sql = "SELECT * FROM vote WHERE userID ='$user_id' AND trailID = '$trail_id'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
-    echo $row['vote'];
+
     if ($row['vote'] == 1) 
     {
       echo "<style>#upvote_trail_button{color: rgb(255, 173, 43)} ";
