1da6e6ed809bcae5dc3ba299b0358ccdcec1d38d
Stig-Tarzan <john.f.almqvist@gmail.com>
2017-05-28 22:42:27 +0200

    Rate

--- includes/views/load_trail.inc.php
+++ includes/views/load_trail.inc.php
@@ -81,37 +81,6 @@ $('#add_trail').css('bottom', '15%');
 
   echo $googlemaps_api;
   echo "<input type='hidden' id='vote_trail_name' value='$trail_name'></input>";
-  echo "<input type='hidden' id='vote_trail_creator_name' value='$trail_creator'></input>";
-  echo "<div id=vote_container>";
-  echo "<i id='upvote_trail_button'class='material-icons-arrows'>arrow_upward</i>";
-  echo "<i id='downvote_trail_button' class='material-icons-arrows'>arrow_downward</i></div>";
-
-
- 
-
-  if (isset($_SESSION['user_name'])) 
-  {
-     $user_id = $_SESSION['user_id'];
-    if($trail_creator == $_SESSION['user_name'] OR $_SESSION['admin'] == 1)
-    {
-      echo "<button id='".$trail_name."' class='delete_trail button'>Radera</button>";
-    }
-
-    $sql = "SELECT * FROM vote WHERE userID ='$user_id' AND trailID = '$trail_id'";
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-    echo $row['vote'];
-    if ($row['vote'] == 1) 
-    {
-      echo "<style>#upvote_trail_button{color: rgb(255, 173, 43)} ";
-      echo "<style>#downvote_trail_button{color: rgb(166, 166, 166)} ";
-    }
-    else if($row['vote'] == -1)
-    {
-      echo "<style>#downvote_trail_button{color: rgb(153, 187, 255)} ";
-      echo "<style>#upvote_trail_button{color: rgb(166, 166, 166)} ";
-    }
-}
   echo "<input type='hidden' id='vote_trail_creator_name' value='$trail_creator'></input>"; 
 
   echo "<label id='trail_name_in_display' value='$trail_name'><p>$trail_name</p></label>";
